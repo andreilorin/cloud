d94f021e1cee508c01c18117db74d94887b24bce
Andrei Lorin <andreilorin@gmail.com>
2020-10-04 19:29:58 +0100

    Add provider variables

--- websites/andreilorin/terraform/provider.tf
+++ websites/andreilorin/terraform/provider.tf
@@ -1,8 +1,21 @@
-variable "do_token" {}
-variable "pub_key" {}
-variable "pvt_key" {}
-variable "ssh_fingerprint" {}
+variable "do_token" {
+  type = string
+}
+variable "pub_key" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
+variable "domain_name" {
+  type = string
+}
+variable "server_name" {
+  type = string
+  default = "test-server"
+}
 
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
+  version = "1.22.2"
 }
