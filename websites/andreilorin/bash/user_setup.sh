bd83f22a4bd7613413f29830c6db6a1569aff1f6
Andrei Lorin <andreilorin@gmail.com>
2020-10-04 14:33:08 +0100

    Complete SSH setup for new user

--- websites/andreilorin/bash/user_setup.sh
+++ websites/andreilorin/bash/user_setup.sh
@@ -1,12 +1,18 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-useradd "$1"
-mkdir /home/"$1"
-mkdir /home/"$1"/.ssh
-chmod 700 /home/"$1"/.ssh
-usermod -s /bin/bash "$1"
-cp .ssh/authorized_keys /home/"$1"/.ssh/authorized_keys
-chmod 400 /home/"$1"/.ssh/authorized_keys
-chown "$1":"$1" /home/andrei/ -R
+if [ -z "$1" ]; then
+  echo "Please specify the new username"
+else
+  useradd -m -s /bin/bash "$1"
+  mkdir /home/"$1"/.ssh
+  chmod 700 /home/"$1"/.ssh
+  cp .ssh/authorized_keys /home/"$1"/.ssh/authorized_keys
+  chmod 400 /home/"$1"/.ssh/authorized_keys
+  chown "$1":"$1" /home/"$1"/ -R
 
-sed -i '/PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
+  rm .ssh/authorized_keys
+
+  sed -i '0,/PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
+fi
+
+systemctl restart ssh
